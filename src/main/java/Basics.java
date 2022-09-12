6e06f09198ba7b45aad5f885b92c9c2d99226f5e
auroraborie <auroraaurora.zhang@mail.utoronto.ca>
2022-09-12 17:19:01 -0400

    finished to do tasks and passed all unit tests given.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,9 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+
+        int my_variable;
+        my_variable = 100;
 
 
 
@@ -99,6 +103,10 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+
+        for(int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,9 +149,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] array = to_split.split(" ");
 
-        // Fill in the rest of the body here
-
+        for(String w: array) {
+            ret.append(w.charAt(0));
+        }
         return ret.toString();
     }
 
@@ -170,6 +180,15 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if(arr.length == 0 || arr.length == 1) {
+            return 0;
+        }
+        for(int i = 0; i < arr.length; i++) {
+            if (i % 2 != 0) {
+                current_sum += arr[i];
+            }
+
+        }
 
         return current_sum;
     }
@@ -184,4 +203,5 @@ public class Basics {
      *                If the tests don't pass, look at the results and revise
      *                accordingly.
      */
+
 }
